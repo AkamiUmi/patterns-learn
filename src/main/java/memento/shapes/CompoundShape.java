f979d27dc9e432e839cd1bf031d703c650c39dd9
akamiumi <akamiumi@gmail.com>
2025-03-23 18:01:13 +0500

    [fix] class has been fixed

--- src/main/java/memento/shapes/CompoundShape.java
+++ src/main/java/memento/shapes/CompoundShape.java
@@ -12,8 +12,112 @@ public class CompoundShape extends BaseShape{
     add(components);
   }
 
+  public void add(Shape component) {
+    children.add(component);
+  }
+
   public void add(Shape... components) {
     children.addAll(Arrays.asList(components));
+  }
+
+  public void remove(Shape child) {
+    children.remove(child);
+  }
+
+  public void remove(Shape... components) {
+    children.removeAll(Arrays.asList(components));
+  }
+
+  public void clear() {
+    children.clear();
+  }
+
+  @Override
+  public int getX() {
+    if (children.size() == 0) {
+      return 0;
+    }
+    int x = children.get(0).getX();
+    for (Shape child : children) {
+      if (child.getY() < y) {
+        y = child.getY();
+      }
+    }
+    return y;
+  }
+
+  @Override
+  public int getWidth() {
+    int maxWidth = 0;
+    int x = getX();
+    for (Shape child : children) {
+      int childSRelativeX = child.getX() - x;
+      int childWidth = childSRelativeX + child.getWidth();
+      if (childWidth > maxWidth) {
+        maxWidth = childWidth;
+      }
+    }
+    return maxWidth;
+  }
+
+  @Override
+  public int getHeight() {
+    int maxHeight = 0;
+    int y = getY();
+    for (Shape child : children) {
+      int childSRelativeY = child.getY() - y;
+      int childHeight = childSRelativeY + child.getHeight();
+      if (childHeight > maxHeight) {
+        maxHeight = childHeight;
+      }
+    }
+    return maxHeight;
+  }
+
+  @Override
+  public void drag() {
+    for (Shape child : children) {
+      child.drag();
+    }
+  }
+
+  @Override
+  public void drop() {
+    for (Shape child : children) {
+      child.drop();
+    }
+  }
+
+  @Override
+  public void moveTo(int x, int y) {
+    for (Shape child : children) {
+      child.moveTo(x, y);
+    }
+  }
+
+  @Override
+  public void moveBy(int x, int y) {
+    for (Shape child : children) {
+      child.moveBy(x, y);
+    }
+  }
+
+  @Override
+  public boolean isInsideBounds(int x, int y) {
+    for (Shape child : children) {
+      if (child.isInsideBounds(x, y)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @Override
+  public void setColor(Color color) {
+    super.setColor(color);
+    for (Shape child : children) {
+      child.setColor(color);
+    }
   }
 
   public List<Shape> getSelected() {
@@ -24,5 +128,43 @@ public class CompoundShape extends BaseShape{
       }
     }
     return selected;
+  }
+
+  @Override
+  public void unSelect() {
+    super.unSelect();
+    for (Shape child : children) {
+      child.unSelect();
+    }
+  }
+
+  public Shape getChildAt(int x, int y) {
+    for (Shape child : children) {
+      if (child.isInsideBounds(x, y)) {
+        return child;
+      }
+    }
+    return null;
+  }
+
+  public boolean selectChildAt(int x, int y) {
+    Shape child = getChildAt(x,y);
+    if (child != null) {
+      child.select();
+      return true;
+    }
+    return false;
+  }
+
+  @Override
+  public void paint(Graphics graphics) {
+    if (isSelected()) {
+      enableSelectionStyle(graphics);
+      graphics.drawRect(getX() - 1, getY() - 1, getWidth() + 1, getHeight() + 1);
+      disableSelectionStyle(graphics);
+    }
+    for (Shape child : children) {
+      child.paint(graphics);
+    }
   }
 }
