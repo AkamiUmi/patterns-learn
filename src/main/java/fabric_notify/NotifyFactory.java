020d278790f9b07f68eb3c1918d31f6555624362
akamiumi <akamiumi@gmail.com>
2025-04-01 11:35:55 +0500

    [fix] initialization

--- src/main/java/fabric_notify/NotifyFactory.java
+++ src/main/java/fabric_notify/NotifyFactory.java
@@ -2,14 +2,18 @@ package fabric_notify;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ServiceLoader;
 
 public class NotifyFactory {
   private static final Map<String, Notifier> notifiersMap = new HashMap<>();
 
   static {
-    notifiersMap.put("sms", new SmsNotifier());
-    notifiersMap.put("telegram", new TelegramNotifier());
-    notifiersMap.put("email", new EmailNotifier());
+    ServiceLoader<Notifier> loader = ServiceLoader.load(Notifier.class);
+    for (Notifier notifier : loader) {
+      System.out.println("Loaded notifier: " + notifier.getClass().getName());
+      String key = notifier.getClass().getSimpleName().toLowerCase().replace("notifier", "");
+      notifiersMap.put(key, notifier);
+    }
   }
 
   public static Notifier getNotifier(String type) {
